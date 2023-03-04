271b708948e2cd7d7a8ff46f0410a9dcc17b23a3
WatanabeTomoki0116 <122767926+WatanabeTomoki0116@users.noreply.github.com>
2023-03-04 16:06:25 +0900

    Update CurrencyWidgetBuilder.js

--- CurrencyWidgetBuilder.js
+++ CurrencyWidgetBuilder.js
@@ -414,7 +414,7 @@ input[type="submit"] {
 <input id="__button210-BDI-content"  value="Update Settings" type="submit" >
 <br>
 
-<p>詳細ブログは <a target="_blank" href="https://blogs.sap.com/2023/03/01/currency-widget-to-display-real-time-currency-conversion-in-sap-analytics-cloud-apps/?source=email-global-notification-bp-new-in-tag-followed">こちら</a></p>
+<p>Referrer blog <a target="_blank" href="https://blogs.sap.com/2023/03/01/currency-widget-to-display-real-time-currency-conversion-in-sap-analytics-cloud-apps/?source=email-global-notification-bp-new-in-tag-followed">Click</a></p>
 </form>
 `;
     class BuilderPanel extends HTMLElement {
